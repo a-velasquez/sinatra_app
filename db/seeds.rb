8752534e0648f9a5247396e7ee22568146361b9f
Alex Velasquez <jtinnesco@gmail.com>
2020-05-03 05:34:47 +0000

    fixed user model and seeded db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,8 @@
-  
+#Create users
+dylan = User.create(username: "Dylan", email: "dylan@dylan.com", password: "password")
+gary = User.create(username: "Gary", email: "gary@gary.com", password: "password")
+
+#Create and Save posts
+Post.create(content: "Starting the first day of 100 days of Code!", user_id: dylan.id)
+
+Post.create(content: "Starting my Flatiron Sinatra Project!", user_id: gary.id)
