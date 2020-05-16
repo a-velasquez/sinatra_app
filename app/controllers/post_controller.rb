841cd344e8eac4e60a6a49ba3e92f372e3d11b7d
Alex Velasquez <jtinnesco@gmail.com>
2020-05-16 23:12:56 +0000

    added validations to posts/new

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -3,16 +3,28 @@ class PostController < ApplicationController
   #create
 
   get '/posts/new' do
-    if logged_in?
-      erb :'/posts/new'
+    if logged_in? #checks to see if user is logged in
+      erb :'/posts/new' #if logged in, lets user see new post form
     else
-      redirect '/login'
+      redirect '/login' #if not logged in, takes user to login page
     end
   end
 
   post '/posts' do #.create also saves the created object, while .build only returns the new object.
-    @post = current_user.posts.create(content: params[:content]) # creates post and sets association.
-    binding.pry
+    if logged_in?
+      if params[:content] == ""
+        redirect "/posts/new"
+      else
+        @post = current_user.posts.create(content: params[:content])# creates post and sets association.
+        if @post
+          redirect "/posts/#{@post.id}"
+        else
+          redirect "/tweets/new"
+        end
+      end
+    else
+      redirect "/login"
+    end
   end
 
   #read all
