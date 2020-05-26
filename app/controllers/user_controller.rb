76d054d5fc537ea200049f9c93a7f41f3507326c
Alex Velasquez <jtinnesco@gmail.com>
2020-05-26 05:30:22 +0000

    added view all posts link to main page

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -11,10 +11,8 @@ class UserController < ApplicationController
 
   post '/signup' do
     if logged_in?
-      # flash[:message] = "You were already logged in. Here are your reviews."
       redirect to '/posts'
     elsif params[:username] == "" || params[:password] == ""
-      # flash[:message] = "In order to sign up for account, you must have both a username & a password. Please try again."
       redirect to '/signup'
     else
       @user = User.create(username: params[:username], password: params[:password])
